2dc03dbdaed2bc1d436911b18dbe48e67de7f389
Nikita Rousseau <nikita.rousseau@etu.unice.fr>
2017-10-20 13:57:55 +0200

    ez

--- Projet Riveill/Datagrid.h
+++ Projet Riveill/Datagrid.h
@@ -4,18 +4,23 @@
 #include "Entity.h"
 
 class Datagrid {
+
 private:
 	Entity* dataGrid[GRID_SIZE_X][GRID_SIZE_Y] = { nullptr }; // Model
+
 public:
 	Datagrid() {
 	}
+
 	~Datagrid() {
 	}
+
 	void setEntityAt(int x, int y, Entity* e) {
 		if (x > 0 && y > 0) {
 			dataGrid[x][y] = e;
 		}
 	}
+
 	Entity* getEntityAt(int x, int y) {
 		if (x > 0 && y > 0) {
 			return dataGrid[x][y];
