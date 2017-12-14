625a1ecdf337e29f9e89bb23b7d5a0f9c709c201
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2017-12-14 21:59:17 +0100

    back to sequential

--- Simulation.cpp
+++ Simulation.cpp
@@ -91,7 +91,7 @@ void Simulation::fill_grid(Entity *e) {
 
 // Compute the next frame
 void *tick(void *arguments) {
-    //pthread_mutex_lock(&simulation_mutex);
+    pthread_mutex_lock(&simulation_mutex);
     struct arg_struct *args = (struct arg_struct *) arguments;
     int nb = args->thread_number;
     Simulation *instance = args->instance;
@@ -104,10 +104,10 @@ void *tick(void *arguments) {
         int new_y = p->get_y();
         if ((old_x == new_x) && (old_y == new_y)) {//We just blocked
             std::cout << "Blocked @" << new_x << ", " << new_y << std::endl;
-            //pthread_cond_broadcast(&cond_var);
+            pthread_cond_broadcast(&cond_var);
             while (p->getNextDestination(instance->dataGrid) != nullptr) {
-                usleep(1);
-                //pthread_cond_wait(&cond_var, &simulation_mutex);
+//                usleep(1);
+                pthread_cond_wait(&cond_var, &simulation_mutex);
             }
             std::cout << "Thread #" << nb << " id:" << pthread_self() << " se reveille" << std::endl;;
         } else {
@@ -115,8 +115,8 @@ void *tick(void *arguments) {
         }
     }
     std::cout << "!!!!!!!Thread #" << nb << " id:" << pthread_self() << " ma personne est sortie!!!!!!!" << std::endl;
-    //pthread_cond_broadcast(&cond_var);
-    //pthread_mutex_unlock(&simulation_mutex);
+    pthread_cond_broadcast(&cond_var);
+    pthread_mutex_unlock(&simulation_mutex);
     return nullptr;
 }
 
