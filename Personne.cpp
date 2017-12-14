e7bd96e38965389af9b8160b693cefb8d80792d4
nikita <nikita.rousseau@etu.unice.fr>
2017-12-14 20:58:25 +0100

    Better Automata

--- Personne.cpp
+++ Personne.cpp
@@ -41,6 +41,19 @@ std::vector<int> Personne::getNextDestination() {
     pos_w[1] = this->get_y();
     pos_w[2] = 2;
 
+    // TEST ESCAPE ZONES
+    if (Command::is_an_escape_zone(pos_nw[0], pos_nw[1])) {
+        return pos_nw;
+    }
+    if (Command::is_an_escape_zone(pos_w[0], pos_w[1])) {
+        return pos_w;
+    }
+    if (Command::is_an_escape_zone(pos_n[0], pos_n[1])) {
+        return pos_n;
+    }
+
+    // =================================================================================================================
+
     // IF NORTH-WEST IS AVAILABLE
     if (!Command::is_oob(pos_nw[0], pos_nw[1], datagrid) && datagrid->getEntityAt(pos_nw[0], pos_nw[1]) == nullptr) {
 
@@ -51,7 +64,8 @@ std::vector<int> Personne::getNextDestination() {
     // IF NORTH IS OUT OF ARENA or OCCUPIED
     if (Command::is_oob(pos_n[0], pos_n[1], datagrid) || (datagrid->getEntityAt(pos_n[0], pos_n[1]) != nullptr)) {
 
-        if (datagrid->getEntityAt(pos_w[0], pos_w[1]) == nullptr) {
+        if (!Command::is_oob(pos_w[0], pos_w[1], datagrid) && datagrid->getEntityAt(pos_w[0], pos_w[1]) == nullptr) {
+
             // GO LEFT
             return pos_w;
         }
@@ -60,7 +74,8 @@ std::vector<int> Personne::getNextDestination() {
     // IF WEST IS OUT OF ARENA or OCCUPIED
     if (Command::is_oob(pos_w[0], pos_w[1], datagrid) || (datagrid->getEntityAt(pos_w[0], pos_w[1]) != nullptr)) {
 
-        if (datagrid->getEntityAt(pos_n[0], pos_n[1]) == nullptr) {
+        if (!Command::is_oob(pos_n[0], pos_n[1], datagrid) && datagrid->getEntityAt(pos_n[0], pos_n[1]) == nullptr) {
+
             // GO NORTH
             return pos_n;
         }
